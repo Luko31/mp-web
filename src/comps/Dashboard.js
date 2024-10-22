dd6326d1287dee0111bb240aa005c60a780d6177
Luko31 <lukasbaran05@gmail.com>
2024-10-22 20:15:40 +0200

    Refactor Dashboard component to use react-router-dom and axios

--- src/comps/Dashboard.js
+++ src/comps/Dashboard.js
@@ -1,15 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Dashboard.css';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';  // Import js-cookie
 
 const Dashboard = () => {
     const [gateStatus, setGateStatus] = useState('closed');
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+        const access_token = Cookies.get('auth_token');
+        if (!access_token) {
+            navigate('/login');
+        }
+    }, [navigate]);
 
     const openGate = async () => {
         try {
-            await axios.post('https://mp-server.seatbook.sk/vehicle', { action: 'open' });
+            const access_token = Cookies.get('auth_token');
+            await axios.get('https://mp.seatbook.sk/api/vehicle/', { 
+                headers: {
+                    Authorization: `JWT ${access_token}`
+                } 
+            });
             setGateStatus('open');
-            
         } catch (error) {
             if (error.response && error.response.status === 401) {
                 Cookies.remove('auth_token');
@@ -21,25 +35,18 @@ const Dashboard = () => {
 
     const closeGate = async () => {
         try {
-            await axios.post('https://mp-server.seatbook.sk/vehicle', { action: 'close' });
+            await axios.post('https://mp.seatbook.sk/api/vehicle/', { action: 'close' });
             setGateStatus('closed');
         } catch (error) {
-            if (error.response && error.response.status === 401) {
-                Cookies.remove('auth_token');
-                navigate('/login');
-            }
-            console.error('Error opening gate:', error);
+            console.error('Error closing gate:', error);
         }
     };
 
     return (
         <div className='dashboard-container'>
             <h1>Dashboard</h1>
-            <section className="button-row">
-            <button className='dashboard-button open' onClick={openGate}>Open Gate</button>
-            <button className='dashboard-button close' onClick={closeGate}>Close Gate</button>
-            <button className='dashboard-button pedest' onClick={pedest}>Open Pedestrain</button>
-            </section>
+            <button className='dashboard-button' onClick={openGate}>Open Gate</button>
+            <button className='dashboard-button' onClick={closeGate}>Close Gate</button>
             <p>Status: Gate is {gateStatus}</p>
         </div>
     );
