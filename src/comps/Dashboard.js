901cfd9f4298857c7b5cae1e1deea4b92129366a
danddorr <120369188+danddorr@users.noreply.github.com>
2025-01-14 08:40:48 +0100

    .

--- src/comps/Dashboard.js
+++ src/comps/Dashboard.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect, useRef } from 'react';
 import './Dashboard.css';
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
+import axios from 'axios';
 
 const Dashboard = () => {
-    const [gateStatus, setGateStatus] = useState('closed');
+    const [gateState, setGateState] = useState('closed');
     const navigate = useNavigate();
     const ws = useRef(null);
 
@@ -12,25 +13,32 @@ const Dashboard = () => {
         const access_token = Cookies.get('auth_token');
         if (!access_token) {
             navigate('/login');
-        } else {
-            ws.current = new WebSocket(`wss://${process.env.REACT_APP_SERVER_DOMAIN}/ws/gate/?token=${access_token}`);
-            ws.current.onmessage = (event) => {
-                const message = JSON.parse(event.data);
-                console.log(message)
-                if (message.message === 'open_v') {
-                    setGateStatus('open for vehicle');
-                } else if (message.message === 'open_p') {
-                    setGateStatus('open for pedestrian');
-                } else if (message.message === 'closed') {
-                    setGateStatus('closed');
-                } else if (message.message === 'not_closed') {
-                    setGateStatus('not closed');
-                }
-            };
-            ws.current.onerror = (error) => {
-                console.error('WebSocket error:', error);
-            };
+            return; // Stop executing the next code
         }
+
+        axios.get(`https://${process.env.REACT_APP_SERVER_DOMAIN}/api/gate-state/`, {
+            headers: {
+              Authorization: `JWT ${access_token}`
+            }
+        }).then(response => {
+            updateGateState(response.data.message);
+        }).catch(() => {
+            console.error('Failed to fetch gate state');
+            updateGateState('unknown');
+        });
+
+        ws.current = new WebSocket(`ws://${process.env.REACT_APP_SERVER_DOMAIN}/ws/gate/?token=${access_token}`);
+        ws.current.onmessage = (event) => {
+            const message = JSON.parse(event.data);
+            console.log(message)
+            if (message.type === 'status') {
+                updateGateState(message.message);
+            }
+        };
+        ws.current.onerror = (error) => {
+            console.error('WebSocket error:', error);
+        };
+        
 
         return () => {
             if (ws.current) {
@@ -41,22 +49,41 @@ const Dashboard = () => {
 
     const openGateVehicle = () => {
         if (ws.current && ws.current.readyState === WebSocket.OPEN) {
-            ws.current.send("start_v");
+            ws.current.send("{\"type\": \"trigger\", \"message\": \"start_v\" }");
         }
     };
 
     const openGatePedestrian = () => {
         if (ws.current && ws.current.readyState === WebSocket.OPEN) {
-            ws.current.send("start_p");
+            ws.current.send("{\"type\": \"trigger\", \"message\": \"start_p\" }");
         }
     };
+
+    function updateGateState(gate_status) {
+        switch (gate_status) {
+            case 'open_v':
+                setGateState('open for vehicle');
+                break;
+            case 'open_p':
+                setGateState('open for pedestrian');
+                break;
+            case 'closed':
+                setGateState('closed');
+                break;
+            case 'not_closed':
+                setGateState('not closed');
+                break;
+            default:
+                setGateState('unknown');
+        }
+    }
 
     return (
         <div className='dashboard-container'>
             <h1>Dashboard</h1>
             <button className='dashboard-button' onClick={openGateVehicle}>Open Gate</button>
             <button className='dashboard-button' onClick={openGatePedestrian}>Open Gate Pedestrian</button>
-            <p>Status: Gate is {gateStatus}</p>
+            <p>Status: Gate is {gateState}</p>
         </div>
     );
 };
